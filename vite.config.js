e863e2bf4b49170b54392c99fbe2768056b6bb9c
hussien shokry <hessinshokry3@gmail.com>
2024-10-21 23:11:51 +0300

    fix deploy issues

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/app.css',
                 'node_modules/bootstrap/dist/css/bootstrap.min.css', // Use Bootstrap from node_modules
+                'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', // Add Bootstrap JS here
                 'node_modules/admin-lte/dist/css/adminlte.min.css', // AdminLTE CSS
             ],
             refresh: true,
